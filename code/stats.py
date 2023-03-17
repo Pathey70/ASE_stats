1cf209720c946ef3b8a32a987d662f65f27aca2a
Nikhil Mehra <nmehra2@ncsu.edu>
2023-03-17 10:52:59 -0400

    closes #1, closes #2; Implemented erf() and gaussian()

---
+++ code/stats.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+
+def erf(x):
+    a1 = 0.254829592
+    a2 = -0.284496736
+    a3 = 1.421413741
+    a4 = -1.453152027
+    a5 = 1.061405429
+    p = 0.3275911
+    sign = 1 if x >= 0 else -1
+    x = abs(x)
+    t = 1.0 / (1.0 + p * x)
+    y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * math.exp(-x * x)
+    return sign * y
+
+
+def gaussian(mu=0, sd=1):
+    return mu + sd * math.sqrt(-2 * math.log2(random.random())) * math.cos(2 * math.pi * random.random())
